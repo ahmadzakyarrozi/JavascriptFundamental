241084ed445bc44ac2133d8c326b741a808c187d
ahmadzakyarrozi <zack.zaka1996@gmail.com>
2020-10-02 13:13:18 +0700

    third commit

---
+++ reactjs/learningapp/src/hari.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import hari from './hari';
+// import './index.css';
+// // import App from './App';
+// import * as serviceWorker from './serviceWorker';
+// const crateClass = require('create-react-class')
+
+// const HariKerja = crateClass({
+//     displayName: "HariKerja",
+//     renderListItem(kerja, i) {
+//         return React.createElement("li", { key: i }, kerja)
+//     },
+
+//     render() {
+//         return React.createElement("ul", {className: "kerja"},
+//         this.props.items.map(this.renderListItem) 
+//     }
+// })
--- reactjs/learningapp/src/index.js
+++ reactjs/learningapp/src/index.js
@@ -1,14 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import hari from './hari';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+const crateClass = require('create-react-class')
+
+
+const judul = React.createElement('h1',null, 'Baked Salmon') //html dimulai untuk create element
+
+// React.createElement("ul", {"className": "ingredients"},
+
+// React.createElement("li", null, "A"),
+// React.createElement("li", null, "B"),
+// React.createElement("li", null, "C"),
+// React.createElement("li", null, "D")
+
+// );
+
+var items = [
+  "A",
+  "B",
+  "C",
+  "D"
+]
+
+//Constructring Element With Data
+
+  React.createElement(
+  "ul",
+  null,
+  {className: "ingredients"},
+  items.map((ingredient, i)=> React.createElement("li", null, {key: i}, ingredient))
+)
+
+//How to Create Component using create class
+const JadwalList = crateClass({
+  displayName : "ingredientsList",
+  render() {
+    return React.createElement("ul", {className : "daily"},
+    React.createElement("li", null, "Senin"),
+    React.createElement("li", null, "Selasa"),
+    React.createElement("li", null, "Rabu"),
+    React.createElement("li", null, "Kamis"),
+    React.createElement("li", null, "Jumat"),
+
+    )
+  }
+})
+
+//Array In Create Class
+const JadwalListArray = crateClass({
+  displayName : "Jadwal List Zaki",
+  render() {
+    return React.createElement("ul", {className : "Schedule"},
+    this.props.itemsJadwal.map((jadwal, i)=>{
+      React.createElement("li", {key : i}, jadwal )
+    })
+    )
+  }
+})
+
+const itemsJadwal = [
+  "SENIN",
+  "SELASA",
+  "RABU",
+  "KAMIS",
+  "JUMAT"
+]
+const list = React.createElement(JadwalList, null, null)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// judul,
+
+list,  
+// React.createElement(JadwalListArray, {itemsJadwal}, null),
+
   document.getElementById('root')
+  // <React.StrictMode>
+    /* <App /> */
+  /* </React.StrictMode>, */
+  
 );
 
 // If you want your app to work offline and load faster, you can change
